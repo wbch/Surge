f132ebbb541f7b9256e5a5013d868e1e514f68d9
wbch <51674759+wbch@users.noreply.github.com>
2022-11-01 15:27:22 +0800

    Update IP-Check.js

--- IP-Check.js
+++ IP-Check.js
@@ -1,3 +1,8 @@
+/*
+ * 由@congcong0806编写
+ * 原地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
+ */
+
 let url = "http://ip-api.com/json"
 
 $httpClient.get(url, function(error, response, data){
