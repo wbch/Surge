8987e16d5e92e4746d7761bef3c16017554ed313
wbch <51674759+wbch@users.noreply.github.com>
2022-11-01 16:57:29 +0800

    Update IP-Check.js

--- IP-Check.js
+++ IP-Check.js
@@ -13,7 +13,7 @@ $httpClient.get(url, function(error, response, data){
     let city = jsonData.city
     let isp = jsonData.isp
   body = {
-    title: "${emoji}${country} - ${city}",
+    title: ` ${emoji}${country} - ${city}`,
     content: ` ${ip}\n ${isp}`,
     icon: "globe.asia.australia.fill"
   }
